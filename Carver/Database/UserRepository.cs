6c0cdbcf4ac9c2f0ec115e0a17b32fa561a5f712
Jiangyi Liu <163300193+L-jIANGYI@users.noreply.github.com>
2026-05-08 23:43:54 +0200

    feat: add UserRepository with login method

---
+++ Carver/Database/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Carver.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Carver.Database
+{
+    internal class UserRepository
+    {
+        public User? Login(string email, string password)
+        {
+            using(SqlConnection conn = DBConnection.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT * FROM Users WHERE Email = @Email AND Password = @Password";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    UserRole role = (UserRole)reader.GetInt32(reader.GetOrdinal("Role"));
+
+                    if (role == UserRole.Administrator)
+                    {
+                        return new Administrator(
+                            reader.GetString(reader.GetOrdinal("Name")),
+                            reader.GetString(reader.GetOrdinal("Email")),
+                            reader.GetString(reader.GetOrdinal("Password"))
+                        );
+                    }
+                    else
+                    {
+                        return new User(
+                            reader.GetString(reader.GetOrdinal("Name")),
+                            reader.GetString(reader.GetOrdinal("Email")),
+                            reader.GetString(reader.GetOrdinal("Password")),
+                            role
+                        );
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
