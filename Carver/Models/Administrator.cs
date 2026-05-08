7a248de0a2490f4092156c8f5ed35d0238a620a8
Jiangyi Liu <163300193+L-jIANGYI@users.noreply.github.com>
2026-05-08 11:04:49 +0200

    feat: add User & Administrator Class

---
+++ Carver/Models/Administrator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carver.Models
+{
+    internal class Administrator : User
+    {
+        public Administrator(string email, string name, string password) : base(email, name, password, "Administrator")
+        {
+        }
+    }
+}
---
+++ Carver/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carver.Models
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public User(string email, string name, string password, string role)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.Role = role;
+        }
+    }
+}
