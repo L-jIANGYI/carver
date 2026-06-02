1cdbb97f3e23507e079f39da4f693b67301278e6
Jiangyi Liu <163300193+L-jIANGYI@users.noreply.github.com>
2026-06-02 12:15:11 +0200

    feat: basic UI for ExperienceForm

---
+++ Carver/Forms/ExperienceForm.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Carver.Forms
+{
+    partial class ExperienceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton6 = new RadioButton();
+            radioButton7 = new RadioButton();
+            radioButton8 = new RadioButton();
+            radioButton9 = new RadioButton();
+            radioButton10 = new RadioButton();
+            radioButton11 = new RadioButton();
+            radioButton12 = new RadioButton();
+            lblUserExperience = new Label();
+            btnExperienceSubmit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            label1.Location = new Point(28, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(524, 27);
+            label1.TabIndex = 0;
+            label1.Text = "Het rijden in een Carver voldoet aan mijn verwachting";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            label2.Location = new Point(28, 201);
+            label2.Name = "label2";
+            label2.Size = new Size(411, 27);
+            label2.TabIndex = 1;
+            label2.Text = "De prijsstelling van de Carver is naar wens";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            label3.Location = new Point(28, 303);
+            label3.Name = "label3";
+            label3.Size = new Size(472, 27);
+            label3.TabIndex = 2;
+            label3.Text = "De extra opties op een Carver vind ik toereikend";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            label4.Location = new Point(28, 399);
+            label4.Name = "label4";
+            label4.Size = new Size(421, 27);
+            label4.TabIndex = 3;
+            label4.Text = "Ik ga snel over tot aanschaf van een Carver";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(28, 143);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(135, 24);
+            radioButton1.TabIndex = 4;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Niet mee eens";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(201, 143);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(92, 24);
+            radioButton2.TabIndex = 5;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Neutraal";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(330, 143);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(101, 24);
+            radioButton3.TabIndex = 6;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Mee eens";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(28, 250);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(135, 24);
+            radioButton4.TabIndex = 8;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Niet mee eens";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(330, 250);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(101, 24);
+            radioButton5.TabIndex = 10;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Mee eens";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(201, 250);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(92, 24);
+            radioButton6.TabIndex = 9;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "Neutraal";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(28, 345);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(135, 24);
+            radioButton7.TabIndex = 12;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "Niet mee eens";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(330, 345);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(101, 24);
+            radioButton8.TabIndex = 14;
+            radioButton8.TabStop = true;
+            radioButton8.Text = "Mee eens";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            radioButton9.AutoSize = true;
+            radioButton9.Location = new Point(201, 345);
+            radioButton9.Name = "radioButton9";
+            radioButton9.Size = new Size(92, 24);
+            radioButton9.TabIndex = 13;
+            radioButton9.TabStop = true;
+            radioButton9.Text = "Neutraal";
+            radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            radioButton10.AutoSize = true;
+            radioButton10.Location = new Point(28, 438);
+            radioButton10.Name = "radioButton10";
+            radioButton10.Size = new Size(135, 24);
+            radioButton10.TabIndex = 16;
+            radioButton10.TabStop = true;
+            radioButton10.Text = "Niet mee eens";
+            radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton11
+            // 
+            radioButton11.AutoSize = true;
+            radioButton11.Location = new Point(330, 438);
+            radioButton11.Name = "radioButton11";
+            radioButton11.Size = new Size(101, 24);
+            radioButton11.TabIndex = 18;
+            radioButton11.TabStop = true;
+            radioButton11.Text = "Mee eens";
+            radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton12
+            // 
+            radioButton12.AutoSize = true;
+            radioButton12.Location = new Point(201, 438);
+            radioButton12.Name = "radioButton12";
+            radioButton12.Size = new Size(92, 24);
+            radioButton12.TabIndex = 17;
+            radioButton12.TabStop = true;
+            radioButton12.Text = "Neutraal";
+            radioButton12.UseVisualStyleBackColor = true;
+            // 
+            // lblUserExperience
+            // 
+            lblUserExperience.AutoSize = true;
+            lblUserExperience.Font = new Font("Microsoft YaHei UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 134);
+            lblUserExperience.Location = new Point(28, 21);
+            lblUserExperience.Name = "lblUserExperience";
+            lblUserExperience.Size = new Size(306, 40);
+            lblUserExperience.TabIndex = 19;
+            lblUserExperience.Text = "Gebruikerservaring";
+            // 
+            // btnExperienceSubmit
+            // 
+            btnExperienceSubmit.Location = new Point(28, 502);
+            btnExperienceSubmit.Name = "btnExperienceSubmit";
+            btnExperienceSubmit.Size = new Size(162, 45);
+            btnExperienceSubmit.TabIndex = 20;
+            btnExperienceSubmit.Text = "Bevestigen";
+            btnExperienceSubmit.UseVisualStyleBackColor = true;
+            // 
+            // ExperienceForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(618, 610);
+            Controls.Add(btnExperienceSubmit);
+            Controls.Add(lblUserExperience);
+            Controls.Add(radioButton10);
+            Controls.Add(radioButton11);
+            Controls.Add(radioButton12);
+            Controls.Add(radioButton7);
+            Controls.Add(radioButton8);
+            Controls.Add(radioButton9);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton5);
+            Controls.Add(radioButton6);
+            Controls.Add(radioButton1);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "ExperienceForm";
+            Text = "ExperienceForm";
+            Load += ExperienceForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private RadioButton radioButton6;
+        private RadioButton radioButton7;
+        private RadioButton radioButton8;
+        private RadioButton radioButton9;
+        private RadioButton radioButton10;
+        private RadioButton radioButton11;
+        private RadioButton radioButton12;
+        private Label lblUserExperience;
+        private Button btnExperienceSubmit;
+    }
+}
---
+++ Carver/Forms/ExperienceForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Carver.Forms
+{
+    public partial class ExperienceForm : Form
+    {
+        public ExperienceForm()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExperienceForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
