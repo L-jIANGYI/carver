64b15cf2c90b8aba43324e65defd7b1f7c009e7f
Jiangyi Liu <163300193+L-jIANGYI@users.noreply.github.com>
2026-06-02 14:00:19 +0200

    feat: add experience repo and service

---
+++ Carver/Database/ExperienceRepository.cs
@@ -0,0 +1,86 @@
+﻿using Carver.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Carver.Database
+{
+    internal class ExperienceRepository
+    {
+        public void Add(Experience experience)
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                conn.Open();
+                string query = @"INSERT INTO Experiences 
+                    (TestDriveId, MeetExpectation, PriceSatisfaction, OptionsAdequate, WillPurchase) 
+                    VALUES 
+                    (@TestDriveId, @MeetExpectation, @PriceSatisfaction, @OptionsAdequate, @WillPurchase)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@TestDriveId", experience.TestDriveId);
+                cmd.Parameters.AddWithValue("@MeetExpectation", (int)experience.MeetExpectation);
+                cmd.Parameters.AddWithValue("@PriceSatisfaction", (int)experience.PriceSatisfaction);
+                cmd.Parameters.AddWithValue("@OptionsAdequate", (int)experience.OptionsAdequate);
+                cmd.Parameters.AddWithValue("@WillPurchase", (int)experience.WillPurchase);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public Experience? GetByTestDriveId(int testDriveId)
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT * FROM Experiences WHERE TestDriveId = @TestDriveId";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@TestDriveId", testDriveId);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    Experience experience = new Experience(reader.GetInt32(reader.GetOrdinal("TestDriveId")))
+                    {
+                        MeetExpectation = (ExperienceAnswer)reader.GetInt32(reader.GetOrdinal("MeetExpectation")),
+                        PriceSatisfaction = (ExperienceAnswer)reader.GetInt32(reader.GetOrdinal("PriceSatisfaction")),
+                        OptionsAdequate = (ExperienceAnswer)reader.GetInt32(reader.GetOrdinal("OptionsAdequate")),
+                        WillPurchase = (ExperienceAnswer)reader.GetInt32(reader.GetOrdinal("WillPurchase"))
+                    };
+                    experience.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+                    return experience;
+                }
+
+                return null;
+            }
+        }
+
+        public List<Experience> GetAll()
+        {
+            List<Experience> experiences = new List<Experience>();
+
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT * FROM Experiences";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Experience experience = new Experience(reader.GetInt32(reader.GetOrdinal("TestDriveId")))
+                    {
+                        MeetExpectation = (ExperienceAnswer)reader.GetInt32(reader.GetOrdinal("MeetExpectation")),
+                        PriceSatisfaction = (ExperienceAnswer)reader.GetInt32(reader.GetOrdinal("PriceSatisfaction")),
+                        OptionsAdequate = (ExperienceAnswer)reader.GetInt32(reader.GetOrdinal("OptionsAdequate")),
+                        WillPurchase = (ExperienceAnswer)reader.GetInt32(reader.GetOrdinal("WillPurchase"))
+                    };
+                    experience.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+                    experiences.Add(experience);
+                }
+            }
+
+            return experiences;
+        }
+    }
+}
---
+++ Carver/Services/ExperienceService.cs
@@ -0,0 +1,30 @@
+﻿using Carver.Database;
+using Carver.Models;
+
+namespace Carver.Services
+{
+    internal class ExperienceService
+    {
+        private readonly ExperienceRepository _repo = new ExperienceRepository();
+
+        public void Add(Experience experience)
+        {
+            _repo.Add(experience);
+        }
+
+        public Experience? GetByTestDriveId(int testDriveId)
+        {
+            return _repo.GetByTestDriveId(testDriveId);
+        }
+
+        public List<Experience> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public bool ExistsForTestDrive(int testDriveId)
+        {
+            return _repo.GetByTestDriveId(testDriveId) != null;
+        }
+    }
+}
